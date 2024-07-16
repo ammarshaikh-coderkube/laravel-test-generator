897d599cb6688e62663222e4360428a50fd04da0
Ammar Shaikh <ammarshaikh.coderkube@gmail.com>
2024-07-16 15:50:25 +0530

    Laravel test generetor

---
+++ src/TestGeneratorServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MrShk7\LaravelTestGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TestGenerator::class
+            ]);
+        }
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
